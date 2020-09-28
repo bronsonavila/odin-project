cc057656521d4ee8d5ece8c19a53f39f7afcae61
Bronson Avila <bronson.avila@gmail.com>
2020-09-28 13:52:17 -1000

    Modify substrings.rb

--- ruby-programming/substrings.rb
+++ ruby-programming/substrings.rb
@@ -1,14 +1,18 @@
 # https://www.theodinproject.com/courses/ruby-programming/lessons/sub-strings
 
-dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+dict = %w[below down go going horn how howdy it i low own part partner sit]
 
-def substrings(str, dictionary)
+def substrings(str, dict)
   str_array = str.downcase.split
-  dictionary.reduce(Hash.new(0)) do |result, entry|
+  dict.reduce(Hash.new(0)) do |result, entry|
     str_array.each { |word| result[entry] += 1 if word.include? entry }
     result
   end
 end
 
-puts substrings('below', dictionary)
-puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+puts substrings('below', dict)
+# {"below"=>1, "low"=>1}
+
+puts substrings("Howdy partner, sit down! How's it going?", dict)
+# {"down"=>1, "go"=>1, "going"=>1, "how"=>2, "howdy"=>1, "it"=>2, "i"=>3,
+# "own"=>1, "part"=>1, "partner"=>1, "sit"=>1}
